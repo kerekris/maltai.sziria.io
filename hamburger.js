77af535bcbffbbf9d6e3e4712bd5d906e622730c
Kerék Krisztián <krisztian.kerek@gmail.com>
2018-11-25 21:30:13 +0100

    mobilra

--- hamburger.js
+++ hamburger.js
@@ -1,8 +1,11 @@
 var hambi = document.getElementById("hamburger");
 var menu = document.getElementById("menu")
+/*
 hambi.onclick = function (){
 menu.style.display = "block";
-};
+}
 menu.onclick = function (){
 menu.style.display = "none";
-};
+}
+*/
+
