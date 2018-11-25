21bb23fe07c214f4ec92e7541a6491da27853a5a
Kerék Krisztián <krisztian.kerek@gmail.com>
2018-11-25 10:34:15 +0100

    first version

---
+++ hamburger.js
@@ -0,0 +1,8 @@
+var hambi = document.getElementById("hamburger");
+var menu = document.getElementById("menu")
+hambi.onclick = function (){
+menu.style.display = "block";
+};
+menu.onclick = function (){
+menu.style.display = "none";
+};
---
+++ hatter_szin.js
@@ -0,0 +1,5 @@
+var x = document.getElementsByClassName("hatter");
+
+for (i=0; i<x.length; i=i+2){
+    x[i].style.backgroundColor = "#202020";
+}    
