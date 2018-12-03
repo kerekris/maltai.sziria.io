f841d823714320074d710e44161734166593d23a
Kerék Krisztián <krisztian.kerek@gmail.com>
2018-12-03 17:21:38 +0100

    hamburger

--- hamburger.js
+++ hamburger.js
@@ -1,11 +1,13 @@
 var hambi = document.getElementById("hamburger");
-var menu = document.getElementById("menu")
-/*
-hambi.onclick = function (){
-menu.style.display = "block";
+var menu = document.getElementById("hamburgerMenu");
+
+hambi.onclick = function(){
+if (menu.style.display == "none")
+{menu.style.display = "block";}
+else {menu.style.display = "none";}
 }
-menu.onclick = function (){
+
+menu.onclick = function(){
 menu.style.display = "none";
 }
-*/
 
